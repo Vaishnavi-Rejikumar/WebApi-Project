9223190a44a6931a301d05690802d85a944e3339
Vaishnavi Rejikumar <135598925+Vaishnavi-Rejikumar@users.noreply.github.com>
2025-02-26 12:39:53 +0530

    Update Employee.cs

--- Web/Models/Entities/Employee.cs
+++ Web/Models/Entities/Employee.cs
@@ -8,14 +8,21 @@ namespace Web.Models.Entities
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int EmployeeID { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "First name can only contain letters.")]
         public required string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
+        [RegularExpression(@"^[A-Za-z]+$", ErrorMessage = "Last name can only contain letters.")]
         public required string LastName { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        [StringLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
         public required string Email { get; set; }
         
         [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number must be 10 digits")]
         public string? PhoneNumber { get; set; }
         
-        [Required]
+        [Required(ErrorMessage = "Hire date is required.")]
         
         public DateOnly HireDate { get; set; }
         public string? JobTitle { get; set; }
